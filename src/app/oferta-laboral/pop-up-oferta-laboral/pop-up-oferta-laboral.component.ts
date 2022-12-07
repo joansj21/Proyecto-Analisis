567703458f850dc02174b91d92cd74c36f536f5c
joansj21 <joansj21@gmail.com>
2022-12-07 12:55:20 -0600

    arreglo

--- src/app/oferta-laboral/pop-up-oferta-laboral/pop-up-oferta-laboral.component.ts
+++ src/app/oferta-laboral/pop-up-oferta-laboral/pop-up-oferta-laboral.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pop-up-oferta-laboral',
@@ -9,7 +9,7 @@ export class PopUpOfertaLaboralComponent implements OnInit {
 
 
   @Input() viejo:String;
-  @Input() nuevoRequisitosDePuesto:String;
+  @Input() nuevo:String;
 
 
   constructor() { }
